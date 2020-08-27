6b6689a034c47596f4758bc8b82219def1b59b05
Amr-Reda <amrreda547@gmail.com>
2020-08-27 16:51:02 +0200

    implement getAllProducts controller

--- modules/product/controller/index.js
+++ modules/product/controller/index.js
@@ -1,4 +1,5 @@
 const Product = require('../model');
+const PAGE_LIMIT = 10;
 
 const createProduct = async (req, res) => {
     try {
@@ -10,9 +11,21 @@ const createProduct = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     try {
-        
+        const page = req.query.page ? req.query.page : 1;
+        delete req.query.page;
+        const options = {
+            skip: ((PAGE_LIMIT * page) - PAGE_LIMIT),
+            limit: PAGE_LIMIT,
+        };
+        const products = await Product.find().skip(options.skip).limit(options.limit)
+        const totalCount = await Product.countDocuments()
+        return res
+            .status(200)
+            .json({ message: 'products loaded successfully', totalCount, products });
     } catch (error) {
-        
+        return res
+            .status(500)
+            .json({ message: 'Internal Server Error', data: error.message });
     }
 };
 
