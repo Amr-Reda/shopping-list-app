231d58ba34dbb6aa415b15e0a7603818a3c8573e
Amr-Reda <amrreda547@gmail.com>
2020-08-27 15:37:45 +0200

    setup shopping list endpoints controllers

---
+++ modules/shoppingList/controller/index.js
@@ -0,0 +1,40 @@
+const ShoppingList = require('../model');
+
+const createShoppingList = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const getShoppingList = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const addProductToShoppingList = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const removeProductToShoppingList = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+module.exports = {
+    createShoppingList,
+    getShoppingList,
+    addProductToShoppingList,
+    removeProductToShoppingList
+};
--- modules/shoppingList/index.js
+++ modules/shoppingList/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+    getShoppingList,
+    createShoppingList,
+    addProductToShoppingList,
+    removeProductToShoppingList
+} = require('./controller');
+
 router.get(
     '/',
-    (req, res) => {
-        res.end('hello world!')
-    }
+    getShoppingList
+);
+
+router.post(
+    '/',
+    createShoppingList
+);
+
+router.post(
+    '/product',
+    addProductToShoppingList
+);
+
+router.delete(
+    '/product',
+    removeProductToShoppingList
 );
 
 module.exports = router;
